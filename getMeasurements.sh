fd16dbb6f0c65fa99d10c2946af7da326fd29253
Mathias Helsen <helsen.mathias@gmail.com>
2019-02-09 12:29:18 +0100

    added aux scripts

--- getMeasurements.sh
+++ getMeasurements.sh
@@ -15,6 +15,10 @@ echo ${timeOfDay} | tr -s '\n' '\t' >> temperature.txt
 echo ${timeOfDay} | tr -s '\n' '\t' >> humidity.txt
 cat soilHumidity.tmp >> humidity.txt
 
+./AdafruitDHT.py 22 17 > airHumidity.tmp
+echo ${timeOfDay} | tr -s '\n' '\t' >> airHumidity.txt
+cat airHumidity.tmp >> airHumidity.txt
+
 #wget 192.168.1.191 -O tmptemperature.txt
 /home/pi/secretGarden/readTemp.py >> temperature.txt
 
@@ -23,14 +27,19 @@ read lines filename < tmp
 
 awk "NR>($lines-12)" temperature.txt > lastHourTemp.txt
 awk "NR>($lines-12)" humidity.txt > lastHourHum.txt
+awk "NR>($lines-12)" airHumidity.txt > lastHourAirHum.txt
 
 if [ $lines -gt 288 ]
 then
 	awk "NR>($lines-288)" temperature.txt > lastDayTemp.txt
 	awk "NR>($lines-288)" humidity.txt > lastDayHum.txt
+	awk "NR>($lines-288)" airHumidity.txt > lastDayAirHum.txt
 else
 	cat temperature.txt > lastDayTemp.txt
 	cat humidity.txt > lastDayHum.txt
+	cat airHumidity.txt > lastDayAirHum.txt
 fi
 
 gnuplot /home/pi/secretGarden/plotData.gplt
+
+./setTemperature.py
